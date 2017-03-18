31a25f48ff4fba41145012081a198bb315e98e24
zhiyingyang2 <zhiyingyang2@creditease.cn>
2017-03-18 11:06:03 +0800

    spring cloud

---
+++ spring-cloud-config-client/src/main/java/me/ervin/tutorial/SpringCloudConfigClientApplication.java
@@ -0,0 +1,29 @@
+package me.ervin.tutorial;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RefreshScope
+public class SpringCloudConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+    }
+
+    @Value("${config.name}")
+    String name = "World";
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello " + name;
+    }
+
+}
