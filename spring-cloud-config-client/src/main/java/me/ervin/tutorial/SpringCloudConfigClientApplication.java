1de21f27b99f426062706560820153a8ce7f1798
zhiyingyang2 <zhiyingyang2@creditease.cn>
2017-03-20 17:07:31 +0800

    add eureka

--- spring-cloud-config-client/src/main/java/me/ervin/tutorial/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/me/ervin/tutorial/SpringCloudConfigClientApplication.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @RefreshScope
+@EnableEurekaClient
 public class SpringCloudConfigClientApplication {
 
     public static void main(String[] args) {
--- spring-cloud-config-server/src/main/java/me/ervin/tutorial/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/me/ervin/tutorial/SpringCloudConfigServerApplication.java
@@ -3,12 +3,14 @@ package me.ervin.tutorial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class SpringCloudConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+    }
 }
